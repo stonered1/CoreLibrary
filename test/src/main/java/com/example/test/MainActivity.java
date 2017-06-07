1171c2337f413b7532f92e1375f7c7c3d52b26e5
dzqiang <446511396@qq.com>
2017-06-07 12:50:51 +0800

    Signed-off-by: dzqiang <446511396@qq.com>

---
+++ test/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.stone.permissions.MPermissionsActivity;
+
+public class MainActivity extends MPermissionsActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

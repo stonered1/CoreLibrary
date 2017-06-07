79cb28cfe2aa3bed18d09c53f7758d80820f6cfe
dzqiang <446511396@qq.com>
2017-06-07 12:52:22 +0800

    Signed-off-by: dzqiang <446511396@qq.com>

--- test/src/main/java/com/example/test/MainActivity.java
+++ test/src/main/java/com/example/test/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends MPermissionsActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }

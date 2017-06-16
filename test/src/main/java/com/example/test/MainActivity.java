1fd9944e725c14ae968aa596bc2048d909b8a2aa
dzqiang <446511396@qq.com>
2017-06-16 10:37:02 +0800

    Signed-off-by: dzqiang <446511396@qq.com>给权限申请写自己的注释

--- test/src/main/java/com/example/test/MainActivity.java
+++ test/src/main/java/com/example/test/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.test;
 
+import android.Manifest;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +12,7 @@ import com.example.stone.toastutil.ToastUtils;
 
 public class MainActivity extends MPermissionsActivity implements View.OnClickListener {
 
-    private Button bt;
+    private Button bt, btPhone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +22,9 @@ public class MainActivity extends MPermissionsActivity implements View.OnClickLi
 
     private void initView() {
         bt = (Button) findViewById(R.id.button);
+        btPhone = (Button) findViewById(R.id.phone_call);
         bt.setOnClickListener(this);
+        btPhone.setOnClickListener(this);
     }
 
     @Override
@@ -33,12 +37,26 @@ public class MainActivity extends MPermissionsActivity implements View.OnClickLi
                         super.run();
                         ToastUtils.showMessage(MainActivity.this, "Stone");
 
-                        
+
                     }
                 }.start();
 
                 break;
+
+            case R.id.phone_call:
+                requestPermission(new String[] {Manifest.permission.CALL_PHONE}, 0x0001);
+                break;
             default:
+                break;
+        }
+    }
+
+    @Override
+    public void permissionSuccess(int requestCode) {
+        super.permissionSuccess(requestCode);
+        switch (requestCode) {
+            case  0x0001:
+                ToastUtils.showMessage(MainActivity.this, "权限成功");
                 break;
         }
     }
